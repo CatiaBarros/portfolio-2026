1c06f1528f80226c7578a74aec9c731b2c6be198
CatiaBarros <63752454+CatiaBarros@users.noreply.github.com>
2026-06-24 20:36:03 +0100

    Updates

--- _app/immutable/chunks/BlF1BMJd.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./C0CKm1xX.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
